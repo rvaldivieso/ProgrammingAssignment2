b8fae728b944057e891366170e5b820a265605f1
Raúl Valdivieso Martínez <rvaldimar@gmail.com>
2014-09-16 19:11:33 -0500

    Assignment2
    Aquí se encuentra el archivo de la asignación del módulo: “programming
    in R”

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function computes the inverse of a matrix 
+## First, creates a matrix in cache
 
-## Write a short comment describing this function
+## this function builds a special matrix cache
 
 makeCacheMatrix <- function(x = matrix()) {
+i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+		}
+		get <- function() x
+		setImatrix <- function(inverse) i <<- inverse
+		getImatrix <- function() i
+		list(set = set, get = get,
+		setImatrix = setImatrix,
+		getImatrix = getImatrix)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special matrix obtained above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getImatrix()
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+		}
+		matrix <- x$get()
+		i <- solve(matrix, ...)
+		x$setImatrix(i)
+		i
+        
 }
